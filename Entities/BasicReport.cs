09a61c1ba0e8827c124b5c4d3eb66ea8b2c5320a
devjitabh <ajitabh.mishra86@gmail.com>
2024-12-18 20:49:25 +0530

    Add files via upload

---
+++ Entities/BasicReport.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AonAPI.Entities
+{
+    public class BasicReport
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public string? Symbol { get; set; }
+        [Column("%Change")]
+        public decimal? Change { get; set; }
+        public decimal? Cost { get; set; }
+        public decimal? TotalCost { get; set; }
+
+}
+}
