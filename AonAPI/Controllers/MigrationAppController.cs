5dbc67d0d2babd9d1b93cec25aff4a3bf79ac41f
Ajitabh Mishra <ajitabh.mishra86@gmail.com>
2024-12-18 23:58:08 +0530

    API Changes

---
+++ AonAPI/Controllers/MigrationAppController.cs
@@ -0,0 +1,37 @@
+﻿using AonAPI.Data;
+using AonAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MigrationAppController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public MigrationAppController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<StocksUSAColumnar>> GetUSAStocksData()
+        {
+            var stockData= await _dataContext.StocksUSAColumnar.Select(a => new StocksUSAColumnar
+            {
+                Id=a.Id,
+                Description=a.Description,
+                Change=a.Change,               
+                Symbol=a.Symbol,
+                Cost=a.Cost,
+                TotalCost=a.TotalCost
+            }).ToListAsync();
+
+            return Ok(stockData);
+        }
+    }
+}
+    
---
+++ AonAPI/Entities/StocksUSAColumnar.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AonAPI.Entities
+{    
+    public class StocksUSAColumnar
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public string? Symbol { get; set; }
+        public string? EX { get; set; }       
+        public DateTime? DATE { get; set; }
+        public string? DAYOFWEEK { get; set; }
+        [Column("Market CapnNEW")]
+        public decimal? MarketCapnNEW { get; set; }
+        [Column("%Change")]
+        public decimal? Change { get; set; }
+        [Column("% In The Money")]
+        public decimal? InTheMoney { get; set; }
+        [Column("% Out Of The Money")]
+        public string?OutOfTheMoney { get; set; }
+        [Column("Cost")]
+        public decimal? Cost { get; set; }
+        [Column("Total Cost")]
+        public decimal? TotalCost { get; set; }
+
+
+    }
+}
